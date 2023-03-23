77e74b0212ef6d5e37bacb5d82dbb8ddf7dee842
GEdO23 <rm99632@fiap.com.br>
2023-03-23 09:49:30 -0300

    adcionado modo manual de inicio

--- metrostation.py
+++ metrostation.py
@@ -27,48 +27,50 @@ stations = [
 
 i = 0
 
-start = True
-end = False
+first = True
+last = False
 goal = stations[len(stations) - 1]
 
 def goto():
     global i
-    global start, end
+    global first, last
     global goal
 
-    if start == True and stations[i] != stations[len(stations) - 1]:
+    if first == True and stations[i] != stations[len(stations) - 1]:
         i += 1
         goal = stations[len(stations) - 1]
 
-    elif end == True and stations[i] != stations[0]:
+    elif last == True and stations[i] != stations[0]:
         i -= 1
         goal = stations[0]
     
     elif stations[i] == stations[len(stations) - 1]:
-        start = False
-        end = True
+        first = False
+        last = True
 
     elif stations[i] == stations[0]:
-        start = True
-        end = False
+        first = True
+        last = False
 
     else:
         print("I didnt quite undestand that")
         sleep(1)
         goto()
 
-while True:
-    if stations[i] != stations[0]:
-        print(f"\nPREVIOUS STATION : {stations[i-1]} : {i-1}")
-    else: 
-        print("\nFIRST STATION")
-    print(f"CURRENT STATION : {stations[i]} : {i}")
-    if stations[i] != stations[len(stations) - 1]:
-        print(f"NEXT STATION : {stations[i+1]} : {i+1}\n")
-    else:
-        print("LAST STATION\n")
-    
-    print(f"STATION GOAL : {goal}")
-    
-    sleep(1)
-    goto()
+start = input()
+if "y" in start:
+    while True:
+        if stations[i] != stations[0]:
+            print(f"\nPREVIOUS STATION : {stations[i-1]} : {i-1}")
+        else: 
+            print("\nFIRST STATION")
+        print(f"CURRENT STATION : {stations[i]} : {i}")
+        if stations[i] != stations[len(stations) - 1]:
+            print(f"NEXT STATION : {stations[i+1]} : {i+1}\n")
+        else:
+            print("LAST STATION\n")
+        
+        print(f"STATION GOAL : {goal}")
+        
+        sleep(1)
+        goto()
